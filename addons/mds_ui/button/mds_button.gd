c03092690d36c45815836f833a3aebdd0d3fde62
mdossan <mdossan@protonmail.com>
2026-08-16 07:26:34 +0200

    ✨ Create juicy `MdsButton`

---
+++ addons/mds_ui/button/mds_button.gd
@@ -0,0 +1,26 @@
+class_name MdsButton extends Button
+
+@export var audio_hover: AudioStream
+@export var audio_click: AudioStream
+
+func _ready() -> void:
+	pivot_offset = size / 2
+	%AudioHover.stream = audio_hover
+	%AudioClick.stream = audio_click
+
+func _on_mouse_entered() -> void:
+	if audio_hover:
+		%AudioHover.play(0.0)
+	var tween: Tween = get_tree().root.create_tween()
+	tween.tween_property(self, "scale", Vector2(1.2, 1.2), .04)
+	tween.tween_property(self, "scale", Vector2(1.1, 1.1), .08)
+
+func _on_mouse_exited() -> void:
+	var tween: Tween = get_tree().root.create_tween()
+	tween.tween_property(self, "scale", Vector2(1.0, 1.0), .1)
+
+func _on_button_down() -> void:
+	if audio_click:
+		%AudioClick.play(0.0)
+	var tween: Tween = get_tree().root.create_tween()
+	tween.tween_property(self, "scale", Vector2(.9, .9), .04)
