59cd535113b1cc19cbd5ac02e8cd1ee508ec6cdb
mdossan <mdossan@protonmail.com>
2026-08-19 09:26:38 +0200

    ✨ Add `disabled` to `MdsButton`

--- addons/mds_ui/button/mds_button.gd
+++ addons/mds_ui/button/mds_button.gd
@@ -9,17 +9,27 @@ func _ready() -> void:
 	%AudioClick.stream = audio_click
 
 func _on_mouse_entered() -> void:
+	if disabled:
+		return
+
 	if audio_hover:
 		%AudioHover.play(0.0)
+
 	var tween: Tween = get_tree().root.create_tween()
 	tween.tween_property(self, "scale", Vector2(1.2, 1.2), .04)
 	tween.tween_property(self, "scale", Vector2(1.1, 1.1), .08)
 
 func _on_mouse_exited() -> void:
+	if disabled:
+		return
+
 	var tween: Tween = get_tree().root.create_tween()
 	tween.tween_property(self, "scale", Vector2(1.0, 1.0), .1)
 
 func _on_button_down() -> void:
+	if disabled:
+		return
+
 	if audio_click:
 		%AudioClick.play(0.0)
 	var tween: Tween = get_tree().root.create_tween()
